2818d39e175d1a2ea41f5a6b5db1d3128818c1f3
mitumial <ace.vortex76@gmail.com>
2024-12-07 17:27:52 -0500

    apply final changes

---
+++ README.md
@@ -0,0 +1,10 @@
+## To-do List
+
+- Upload to github
+- Use .html (inside intellij) to test http requests
+- Write proper README
+- Create class Currency with exchange-api attr as base
+- Briefly try out other exchange rate apis
+- Create and test request and response to API
+- Convert from json to class
+- Convert to class from json
---
+++ src/main/Main.java
@@ -0,0 +1,72 @@
+package main;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        JsonParser jp = new JsonParser();
+
+        String apiKey = "775dbf773d4aa324d7410628";
+        String strUrlApi = "https://v6.exchangerate-api.com/v6/"+ apiKey + "/pair/";
+
+        Map<String, String> currencyCodes = new HashMap<String, String>();
+        currencyCodes.put("colombia","COP");
+        currencyCodes.put("united states","USD");
+        currencyCodes.put("argentina","ARS");
+        currencyCodes.put("europe","EUR");
+        currencyCodes.put("canada","CAD");
+        currencyCodes.put("egypt", "EGP");
+
+        while (true){
+            System.out.println("Convert from ");
+            System.out.println(currencyCodes.values());
+            String from_choice = scanner.next().trim().toUpperCase();
+
+            System.out.println("Convert to ");
+            System.out.println(currencyCodes.values());
+            String to_choice = scanner.next().trim().toUpperCase();
+            System.out.println("Type corresponding amount");
+            double qty = Double.parseDouble(scanner.next().trim());
+            try {
+                URL urlApi = new URL(strUrlApi+from_choice+"/"+to_choice+"/"+qty);
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(strUrlApi+from_choice+"/"+to_choice+"/"+qty))
+                        .build();
+                HttpResponse<InputStream> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+                JsonElement root = jp.parse(new InputStreamReader(response.body()));
+                JsonObject jsonResponse = root.getAsJsonObject();
+                String strResponse = jsonResponse.get("conversion_result").getAsString();
+                System.out.println(strResponse + " " + to_choice);
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println("Do you wish to continue? (Y/N)");
+                String flag = scanner.next();
+                if (flag.equalsIgnoreCase("y")){
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
